2674b004f875c634e6be0620a8d41a3997a4efcf
Jaymo <jaymo.averay@gmail.com>
2020-08-19 23:01:32 +0930

    Initial Push
    Initial push of files as previously seen in https://github.com/jaymo1011/racing/

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'adamant'
+game 'gta5'
+
+client_scripts {
+	"MissionJSON.lua",
+	"client.lua",
+}
+server_scripts {
+	"MissionJSON.lua",
+	"URLHandler.lua",
+	"server.lua",
+} 
+
+author 'Jaymo'
+version '0.2.0'
+description 'A FiveM resource for fetching, caching, deserialising and loading MissionJSON such as races and other GTA:O UGC'
